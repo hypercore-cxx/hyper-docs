1907233af070f5f362da1d85a1408d67b403c943
heapwolf <paolo@async.ly>
2019-05-28 12:37:49 +0200

    add linter

--- parse.js
+++ parse.js
@@ -73,7 +73,7 @@ module.exports = source => {
 
         last = lastMethod = output.types[lastType] = {
           raw: words.join(' '),
-          params: parseParams(line) 
+          params: parseParams(line)
         }
 
         break
